3d2b504fa90533d625621228e71f89b98e93f04e
Lea3752n <leandrosaucedo2208@gmail.com>
2026-04-16 09:18:31 -0300

    nose

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
+import { Productos } from './paginas/productos/productos';
+import { Contacto } from './paginas/contacto/contacto';
+import { Ofertas } from './paginas/ofertas/ofertas';
+import { Carrito } from './compartidos/carrito/carrito';
+import { Favoritos } from './compartidos/favoritos/favoritos';
+import { InicioSesion } from './auth/inicio-sesion/inicio-sesion';
+import { Registro } from './auth/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+    {path: 'inicio', component: Inicio},
+    {path: 'prods', component: Productos},
+    {path: 'contacto', component: Contacto},
+    {path: 'ofertas', component: Ofertas},
+    {path: 'carro', component: Carrito},
+    {path: 'favoritos', component: Favoritos},
+    {path: 'sesion', component: InicioSesion},
+    {path: 'registro', component: Registro},
+    {path: '**', redirectTo: 'inicio'}
+
+];
